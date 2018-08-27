095dad4e787a2a4305931cb7cdddb57862140e6f
Alex Bogovich <bogovich.alexander@gmail.com>
2018-08-27 10:53:08 +0700

    slice from 27.08

--- src/HOC/withBuilderExtension.tsx
+++ src/HOC/withBuilderExtension.tsx
@@ -1,24 +1,32 @@
 import * as React from 'react'
-// import  from 'react'
 import { getDisplayName } from 'recompose'
 import BuilderMenu from '../components/Builder/BuilderMenu/BuilderMenu'
 import { objectToReactComponent } from '../utils/MappingConverter/MappingConverter'
 import { BehaviorSubject, combineLatest, EMPTY, Observable, Subject } from 'rxjs'
 import { distinctUntilChanged, map, scan, switchMap, tap } from 'rxjs/operators'
 import { isEqual } from 'underscore'
+// import { isArray } from 'rxjs/internal-compatibility'
 
 interface IWithBuilderExtensionProps {
     subject$: Subject<any>,
-    // child$: Subject<any> | Array<Subject<any>>
     settings: any,
-
+    children?: IOverriddenProps | IOverriddenProps[]
+    type: string | IBuilderModule
     [x: string]: any
+}
+
+export interface IBuilderModule {
+    module: string,
+    name?: string,
+    defaultImport?: boolean
 }
 
 export interface IOverriddenProps {
     children?: IOverriddenProps | IOverriddenProps[]
-    props: any,
-    type: any
+    props?: {
+        [x: string]: any
+    },
+    type: string | IBuilderModule
 }
 
 interface IWithBuilderExtensionState {
@@ -29,6 +37,36 @@ interface IWithBuilderExtensionState {
     childrenChanged$?: Observable<any>
     overriddenProps?: IOverriddenProps
     innerSubject$?: Subject<any>
+}
+
+interface IChildSubscriptionWithProps {
+    child$: Subject<IOverriddenProps> | Array<Subject<IOverriddenProps>>,
+    overriddenProps: IOverriddenProps
+}
+
+interface IInternalSubscription {
+    overriddenProps: IOverriddenProps
+    child$: Subject<any> | Array<Subject<any>>
+    latestChildProps: string | IOverriddenProps | IOverriddenProps[]
+}
+
+function mapObjectToOverriddenProps(obj): IOverriddenProps {
+    console.debug('[MAP_CHILD_OBJECT]', obj)
+    const { props, type, children, ...restProps } = obj
+    return {
+        type,
+        children,
+        props: {
+            ...props,
+            ...restProps
+        }
+    }
+}
+
+function createDistinctSubject(value: IOverriddenProps) {
+    return new BehaviorSubject<IOverriddenProps>(value).pipe(
+        distinctUntilChanged((x, y) => isEqual(x, y))
+    )
 }
 
 const withBuilderExtension = WrappedComponent =>
@@ -53,23 +91,7 @@ const withBuilderExtension = WrappedComponent =>
             }
         }
 
-        mapChildObject = child => {
-            console.debug('[MAP_CHILD_OBJECT]', child)
-            const { props, type, children, ...restProps } = child
-            return {
-                type,
-                children,
-                props: {
-                    ...props,
-                    ...restProps
-                }
-            }
-        }
-
-        createDistinctSubject = v =>
-            new BehaviorSubject(v).pipe(distinctUntilChanged((x, y) => isEqual(x, y)))
-
-        scanPropsChanges = (prev, props) => {
+        scanPropsChanges = (prev: IChildSubscriptionWithProps, props: IOverriddenProps): IChildSubscriptionWithProps => {
             console.debug('[CHILDREN_CHANGED_SCAN] process', props, prev)
             const children = (props && props.children) || undefined
             const prev$ = (prev && prev.child$) || undefined
@@ -84,31 +106,77 @@ const withBuilderExtension = WrappedComponent =>
             let child$
             if (Array.isArray(children)) {
                 console.debug('[CHILDREN_CHANGED_SCAN] is array')
-                const prevLength = (prev$ && prev$.length) || 0
+                const prevLength = (Array.isArray(prev$) && prev$.length) || 0
                 if (prevLength === children.length) {
                     console.debug('[CHILDREN_CHANGED_SCAN] same size')
                     children.forEach((v, i) => prev$[i].next(v))
                     child$ = prev$
                 } else if (prevLength > children.length) {
                     console.debug('[CHILDREN_CHANGED_SCAN] new is lower')
-                    child$ = prev$.slice(0, children.length)
+                    child$ = Array.isArray(prev$) && prev$.slice(0, children.length) || []
                     children.forEach((v, i) => child$[i].next(v))
                 } else if (prevLength < children.length) {
                     console.debug('[CHILDREN_CHANGED_SCAN] new is longer')
-                    child$ = (prev$ && [...prev$]) || []
+                    child$ = Array.isArray(prev$) && [...prev$] || []
                     children.forEach(
                         (v, i) =>
                             i < prevLength
                                 ? child$[i].next(v)
-                                : child$.push(this.createDistinctSubject(v))
+                                : child$.push(createDistinctSubject(v))
                     )
                 }
             } else if (children) {
                 console.debug('[CHILDREN_CHANGED_SCAN] is object')
-                child$ = prev$ || this.createDistinctSubject(children)
+                child$ = prev$ || createDistinctSubject(children)
                 child$.next(children)
             }
             return { child$, overriddenProps: props }
+        }
+
+        switchToLatestSubscriptionOfChild = (propsWithChild$: IChildSubscriptionWithProps): Observable<IInternalSubscription> => {
+            console.debug('[INNER_SUBJECT_PIPE$] before switch', propsWithChild$)
+            const { overriddenProps, child$ } = propsWithChild$
+            console.debug('[INNER_SUBJECT_PIPE$] child$ is ', child$)
+            const latest$ = Array.isArray(child$)
+                ? combineLatest(...child$)
+                : child$
+            if (!latest$) return EMPTY
+
+            return latest$.pipe(
+                tap(v =>
+                    console.debug('[INNER_SUBJECT_PIPE$] latest slice in switch', v)
+                ),
+                distinctUntilChanged(),
+                map(latestChildProps => ({
+                    overriddenProps,
+                    child$,
+                    latestChildProps
+                }))
+            )
+        }
+
+        onInternalChangesSubscription = (externalSubject$: Subject<IOverriddenProps>) => (state: IInternalSubscription) => {
+            console.debug('[INNER_SUBJECT_SWITCH$] receive', state)
+            const { overriddenProps, child$, latestChildProps } = state
+            let children
+            if (typeof latestChildProps === 'string') {
+                children = latestChildProps
+            } else {
+                children = Array.isArray(latestChildProps)
+                    ? latestChildProps.map(v => mapObjectToOverriddenProps(v))
+                    : mapObjectToOverriddenProps(latestChildProps)
+            }
+
+            const innerPropsSub = {
+                ...overriddenProps,
+                ...(overriddenProps && overriddenProps.props || undefined),
+                children
+            }
+            console.debug('[INNER_SUBJECT_SWITCH$] set', innerPropsSub)
+            this.setState({ overriddenProps: innerPropsSub, child$ })
+            // TODO: notify with delay. Probably use debounce with some key which can determinate is is source of changes or not
+            console.debug('[INNER_SUBJECT_SWITCH$] notify parent', innerPropsSub)
+            externalSubject$.next(innerPropsSub)
         }
 
         componentDidMount() {
@@ -118,59 +186,19 @@ const withBuilderExtension = WrappedComponent =>
             if (!subject$) {
                 console.warn('[COMPONENT_DID_MOUNT] component dont receive  subject$')
             }
-            const externalSubject$ = subject$ || this.createDistinctSubject(props)
-            const innerSubject$ = new BehaviorSubject<any>(props)
+
+            const innerProps = mapObjectToOverriddenProps(props)
+            const externalSubject$ = subject$ || createDistinctSubject(innerProps)
+            const innerSubject$ = new BehaviorSubject<IOverriddenProps>(innerProps)
 
             innerSubject$.pipe(
                 tap(v => console.debug('[INNER_SUBJECT_$] receive', v)),
                 distinctUntilChanged(),
                 scan(this.scanPropsChanges, undefined),
                 tap(v => console.debug('[INNER_SUBJECT_$] init', v)),
-                switchMap(propsWithChild$ => {
-                    console.debug('[INNER_SUBJECT_PIPE$] before switch', propsWithChild$)
-                    const { overriddenProps, child$ } = propsWithChild$
-                    console.debug('[INNER_SUBJECT_PIPE$] child$ is ', child$)
-                    const latest$ = Array.isArray(child$)
-                        ? combineLatest(...child$)
-                        : child$
-                    if (!latest$) return EMPTY
-
-                    return latest$.pipe(
-                        tap(v =>
-                            console.debug('[INNER_SUBJECT_PIPE$] latest slice in switch', v)
-                        ),
-                        distinctUntilChanged(),
-                        map(latestChildProps => ({
-                            overriddenProps,
-                            child$,
-                            latestChildProps
-                        }))
-                    )
-                }),
+                switchMap(this.switchToLatestSubscriptionOfChild),
                 tap(v => console.debug('[INNER_SUBJECT_PIPE$] latest slice outer', v))
-            ).subscribe((state: {overriddenProps: any, child$: any, latestChildProps: any}) => {
-                console.debug('[INNER_SUBJECT_SWITCH$] receive', state)
-                const { overriddenProps = {}, child$, latestChildProps } = state
-                let children
-                if (typeof latestChildProps === 'string') {
-                    children = latestChildProps
-                } else {
-                    children = Array.isArray(latestChildProps)
-                        ? latestChildProps.map(v => this.mapChildObject(v))
-                        : this.mapChildObject(latestChildProps)
-                }
-
-                const innerProps = {
-                    ...overriddenProps,
-                    ...overriddenProps.props,
-                    children
-                }
-                console.debug('[INNER_SUBJECT_SWITCH$] set', innerProps)
-                this.setState({ overriddenProps: innerProps, child$ })
-                // TODO: notify with delay. Probably use debounce with some key which can determinate is is source of changes or not
-                console.debug('[INNER_SUBJECT_SWITCH$] notify parent', innerProps)
-                externalSubject$.next(innerProps)
-            })
+            ).subscribe(this.onInternalChangesSubscription(externalSubject$))
 
             externalSubject$.subscribe(v => {
                 console.debug('[CDM OWN SUBJECT]', v)
@@ -207,7 +235,7 @@ const withBuilderExtension = WrappedComponent =>
             console.debug('[RENDER]', overriddenProps, this.props)
             const wrappedComponentProps = {
                 ...overriddenProps,
-                // ...overriddenProps.props,
+                // ...(overriddenProps && overriddenProps.props || undefined),
                 children: overriddenProps && objectToReactComponent(overriddenProps.children, child$) || undefined
             }
 
